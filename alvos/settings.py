a2e8d29e424b707a5357f471a25323ebd74bbb64
rickdeu <hangaloandre@gmail.com>
2020-12-27 01:17:08 +0100

    atualizações

--- alvos/settings.py
+++ alvos/settings.py
@@ -75,16 +75,16 @@ WSGI_APPLICATION = 'alvos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-'''DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}'''
+}
 
 
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'alvos',
@@ -93,7 +93,7 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '',
     }
-}
+}'''
 
 
 
